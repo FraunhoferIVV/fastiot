f4f8d447cdcbbda0622258104f8c69d4dd4b01bc
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-04-21 14:27:08 +0200

    Small bugfix to fastiot.cli

--- src/fastiot/cli/__init__.py
+++ src/fastiot/cli/__init__.py
@@ -3,9 +3,6 @@ Methods and classes within this directory are mainly for setting up new projects
 locally or with the Jenkins pipeline
 """
 from fastiot.cli import version
-from fastiot.cli.exceptions import ResolveError
-from fastiot.cli.make_setup_fn import make_setup, find_modules, \
-    CustomModuleConfig, ModulePackageConfig
 
 try:
     from fastiot.cli.__version__ import __version__
