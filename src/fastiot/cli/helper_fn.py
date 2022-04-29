b27e9de7c78273bcff801b2229c43b2cbb3ae84a
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-04-29 17:45:40 +0200

    Adding data model and importer for module manifest

---
+++ src/fastiot/cli/helper_fn.py
@@ -0,0 +1,5 @@
+import logging
+
+
+def get_cli_logger():
+    return logging.getLogger("fastiot.cli")
---
+++ src/fastiot_tests/cli/test_manifest_model.py
@@ -0,0 +1,45 @@
+import unittest
+from tempfile import NamedTemporaryFile
+
+from fastiot.cli.model.manifest import read_manifest, ModuleManifest
+
+MANIFEST = """
+fastiot_module:
+  name: data_dashboard
+
+  mount_config_dir: required
+
+  ports:
+    http:
+      location: 5802
+      env_variable: SAM_DASHBOARD_PORT
+
+  depends_on:
+    - mongodb
+    - nats
+
+  platforms: [ amd64, arm64 ]
+
+"""
+
+
+class TestManifest(unittest.TestCase):
+
+    def test_yaml_parsing(self):
+
+        with NamedTemporaryFile(mode="w") as file:
+            file.write(MANIFEST)
+            file.seek(0)
+            manifest = ModuleManifest.from_yaml_file(filename=file.name)
+            self.assertIsInstance(manifest, ModuleManifest)
+
+    def test_docker_name_parser(self):
+
+        invalid_docker_names = [';do_stuff.sh;', 'image:latest&do-stuff']
+        for invalid_name in invalid_docker_names:
+            with self.assertRaises(ValueError):
+                ModuleManifest.from_docker_image(invalid_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
