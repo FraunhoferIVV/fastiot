2e06ae192be1640139e8e59227554fa3fdc850c7
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-10 19:15:23 +0200

    Preparing for first build
    Creation of dockerfiles working up to debug stage, nuitka compiler still outstanding

--- configure.py
+++ configure.py
@@ -1,6 +1,7 @@
 from fastiot.cli.model import ModulePackageConfig
 
 #extensions = ['fastiot.ext.print']
+
 project_namespace = 'fastiot'
-module_packages = []
+module_packages = [ModulePackageConfig(package_name='fastiot_sample_services')]
 
