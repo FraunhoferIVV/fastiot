4d30d751ca38281d8fa6bd9444c12d17edebf127
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-17 14:46:02 +0200

    Adding correct versioning to fastiot

--- src/fastiot/__init__.py
+++ src/fastiot/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.0'
+from fastiot.cli.version import get_version
+
+try:
+    from fastiot.__version__ import __version__
+except ImportError:
+    __version__ = get_version(complete=True)
