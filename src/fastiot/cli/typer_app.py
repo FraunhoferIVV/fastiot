7afdcb04b96ce7f587ee7a8039680570ad94b5ec
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-17 17:38:38 +0200

    Adding create subcommand for cli

--- src/fastiot/cli/typer_app.py
+++ src/fastiot/cli/typer_app.py
@@ -5,3 +5,6 @@ DEFAULT_CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 app = typer.Typer(
     context_settings=DEFAULT_CONTEXT_SETTINGS
 )
+
+create = typer.Typer(context_settings=DEFAULT_CONTEXT_SETTINGS)
+# Use this command to create any subcommand of create, like `fastiot.cli create my-special-file`
