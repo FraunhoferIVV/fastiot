f8e947b13ec3e9f6ca24a45df20a0926f99e411f
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-21 14:05:20 +0200

    Adding stub of configure command to test build pipeline

---
+++ src/fastiot/cli/common/commands/config.py
@@ -0,0 +1,37 @@
+import os
+from typing import Optional
+
+import typer
+
+from fastiot.cli.common.commands.run import _environment_completion
+from fastiot.cli.constants import GENERATED_DEPLOYMENTS_DIR
+from fastiot.cli.helper_fn import get_jinja_env
+from fastiot.cli.model.context import get_default_context
+from fastiot.cli.typer_app import app
+
+
+@app.command()
+def config(deployment_name: Optional[str] = typer.Argument(default=None, autocompletion=_environment_completion,
+                                                           help="Select the environment to start."),
+           tag: Optional[str] = typer.Option("latest"),
+           docker_net_name: Optional[str] = typer.Option("fastiot_net"),
+           mount_service_ports: Optional[bool] = typer.Option(False)):
+    """ TODO: This implements only the very basics to get the build pipeline started. Actual implementation needed. """
+
+    project_config = get_default_context().project_config
+
+    if deployment_name is None:
+        deployment_names = project_config.get_all_deployment_names()
+    else:
+        deployment_names = [deployment_name]
+
+    for deployment_name in deployment_names:
+        deployment_dir = os.path.join(project_config.project_root_dir, GENERATED_DEPLOYMENTS_DIR, deployment_name)
+        try:
+            os.makedirs(deployment_dir)
+        except FileExistsError:
+            pass  # No need to create directory twice
+
+        with open(os.path.join(deployment_dir, 'docker-compose.yaml'), "w") as docker_compose_file:
+            docker_compose_template = get_jinja_env().get_template('docker-compose.yaml.jinja')
+            docker_compose_file.write(docker_compose_template.render(docker_net_name=docker_net_name))
