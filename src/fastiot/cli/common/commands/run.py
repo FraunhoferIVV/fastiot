a5dbf5eb627fa53471c44450353c6146575157e8
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-21 14:08:26 +0200

    Adding run and stop commands to locally spin up deployments

---
+++ src/fastiot/cli/common/commands/run.py
@@ -0,0 +1,51 @@
+import logging
+import os
+import subprocess
+import sys
+from typing import Optional, List
+
+import typer
+
+from fastiot.cli.constants import GENERATED_DEPLOYMENTS_DIR
+from fastiot.cli.model.context import get_default_context
+from fastiot.cli.typer_app import run_cmd, DEFAULT_CONTEXT_SETTINGS
+
+
+def _environment_completion() -> List[str]:
+    return get_default_context().project_config.get_all_deployment_names()
+
+
+@run_cmd.command(context_settings=DEFAULT_CONTEXT_SETTINGS)
+def environment(environment_name: Optional[str] = typer.Argument(default=None, autocompletion=_environment_completion,
+                                                                 help="Select the environment to start."),
+                service_names: Optional[List[str]] = typer.Argument(default=None,
+                                                                    help="Optionally specify services to be started "
+                                                                         "from the environment."),
+                detach: Optional[bool] = typer.Option(False, "-d", "--detach", help="Use to run this task in the "
+                                                                                    "background (detached)"),
+                project_name: Optional[str] = typer.Option(None, help="Manually set project name for docker-compose")):
+    """ Starts up the selected environment.
+    Be aware, that the configuration needs to be built manually before using `fastiot.cli configure`."""
+    project_config = get_default_context().project_config
+
+    cmd = []
+    cmd.append("docker-compose")
+    project_name = project_name or project_config.project_namespace + "__" + environment_name
+    cmd.append(" --project-name " + project_name)
+
+    cmd.append("up")
+    if detach:
+        cmd.append("-d")
+
+    if service_names is not None:
+            cmd += service_names
+
+    cwd = os.path.join(project_config.project_root_dir, GENERATED_DEPLOYMENTS_DIR,
+                       environment_name)
+    logging.debug("Running command to start the environment: %s", " ".join(cmd))
+    os.environ['COMPOSE_HTTP_TIMEOUT'] = '300'
+    exit_code = subprocess.call(cmd, cwd=cwd)
+    if exit_code != 0:
+        logging.error("Running the environment failed with exit code %s", str(exit_code))
+        sys.exit(exit_code)
+
---
+++ src/fastiot/cli/common/commands/stop.py
@@ -0,0 +1,42 @@
+""" Implements some stop commands for e.g. environments. """
+import logging
+import os
+import subprocess
+from typing import Optional, List
+
+import typer
+
+from fastiot.cli.common.commands.run import _environment_completion
+from fastiot.cli.constants import GENERATED_DEPLOYMENTS_DIR
+from fastiot.cli.model.context import get_default_context
+from fastiot.cli.typer_app import DEFAULT_CONTEXT_SETTINGS, stop_cmd
+
+
+@stop_cmd.command(context_settings=DEFAULT_CONTEXT_SETTINGS)
+def environment(environment_name: Optional[str] = typer.Argument(default=None, autocompletion=_environment_completion,
+                                                                 help="Select the environment to stop."),
+                service_names: Optional[List[str]] = typer.Argument(default=None,
+                                                                    help="Optionally specify services to be stopped "
+                                                                         "from the environment. This will only stop "
+                                                                         "the services but not remove the containers."
+                                                                         "(docker-compose stop instead of down)")
+                ):
+    """
+    Stops up the selected environment.
+    """
+    project_config = get_default_context().project_config
+
+    cmd = "docker-compose "
+
+    if service_names is not None:
+        cmd += "stop" + " ".join(service_names)
+    else:
+        cmd += "down"
+
+    cwd = os.path.join(project_config.project_root_dir, GENERATED_DEPLOYMENTS_DIR,
+                       environment_name)
+    logging.debug("Running command to stop the environment: %s", cmd)
+    os.environ['COMPOSE_HTTP_TIMEOUT'] = '300'
+    exit_code = subprocess.call(f"{cmd}".split(), cwd=cwd)
+    if exit_code != 0:
+        logging.error("Running the environment failed with exit code %s", str(exit_code))
