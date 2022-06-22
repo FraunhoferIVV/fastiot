7cc711c3e68692feaaa2a7e6db436247c77a1b2e
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-22 18:46:18 +0200

    Properly importing print command into cli

--- src/fastiot/cli/commands/__init__.py
+++ src/fastiot/cli/commands/__init__.py
@@ -10,5 +10,6 @@ from fastiot.cli.commands.clean import clean
 from fastiot.cli.commands.config import config
 from fastiot.cli.commands.generate import new_project, new_module
 from fastiot.cli.commands.nuitka_compile import nuitka_compile
+from fastiot.cli.commands.print import print_cmd  # noqa  # pylint: disable=unused-import
 from fastiot.cli.commands.run import environment, tests
 from fastiot.cli.commands.stop import environment
