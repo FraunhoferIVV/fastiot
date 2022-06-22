32fcd6b94399de136717fb702dc29ee04f0c6200
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-06-22 14:08:51 +0200

    Chaning import of commands to work in compiled libraries

---
+++ src/fastiot/cli/common/commands/__init__.py
@@ -0,0 +1,7 @@
+from fastiot.cli.common.commands.build import *
+from fastiot.cli.common.commands.clean import *
+from fastiot.cli.common.commands.config import *
+from fastiot.cli.common.commands.nuitka_compile import *
+from fastiot.cli.common.commands.print import *
+from fastiot.cli.common.commands.run import *
+from fastiot.cli.common.commands.stop import *
