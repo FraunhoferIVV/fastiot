70aba51e5b9f441ee1024149d5a5f63f32d8af7f
Konstantin Merker <konstantin.merker@ivv-dd.fraunhofer.de>
2022-06-30 10:23:46 +0200

    simplified configure.py

--- configure.py
+++ configure.py
@@ -5,9 +5,7 @@ extensions = ['fastiot_ivv_tools.extension']
 project_namespace = 'fastiot'
 
 library_package = 'fastiot'
-modules = [
-    *find_modules(package='fastiot_sample_services')
-]
+modules = find_modules(package='fastiot_sample_services')
 
 test_package = 'fastiot_tests'
 test_config = 'fastiot_test_env'
