397b61de17154a6a7d94df1543b3c216cb66715b
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-07-15 14:24:54 +0200

    Adding some comfort functions to run command
    * If the configure command was not run before => do it
    * If the user wants also start and stop the integration test deployment
    * Ctrl+C when running an deployment in foreground now also calls the stop command to stop and! remove the containers

--- src/fastiot/cli/commands/run.py
+++ src/fastiot/cli/commands/run.py
@@ -19,14 +19,15 @@ def _deployment_completion() -> List[str]:
 def deployment(deployment_name: Optional[str] = typer.Argument(default=None, shell_complete=_deployment_completion,
                                                                help="Select the environment to start."),
                service_names: Optional[List[str]] = typer.Argument(default=None,
-                                                                    help="Optionally specify services to be started "
-                                                                         "from the environment."),
-               detach: Optional[bool] = typer.Option(False, "-d", "--detach", help="Use to run this task in the "
-                                                                                    "background (detached)"),
+                                                                   help="Optionally specify services to be started "
+                                                                        "from the environment."),
+               detach: Optional[bool] = typer.Option(False, "-d", "--detach",
+                                                     help="Use to run this task in the background (detached)"),
                project_name: Optional[str] = typer.Option(None, help="Manually set project name for docker-compose"),
-               run_test_deployment: Optional[bool] = typer.Option(False, help="Explicitly set the environment to the "
-                                                                        "test environment specified in the project. "
-                                                                        "Useful for the CI runner")
+               run_test_deployment: Optional[bool] = typer.Option(False,
+                                                                  help="Explicitly set the environment to the test "
+                                                                       "environment specified in the project. Useful "
+                                                                       "for the CI runner")
                ):
     """ Starts up the selected environment.
     Be aware, that the configuration needs to be built manually before using `fastiot.cli configure`."""
@@ -34,6 +35,10 @@ def deployment(deployment_name: Optional[str] = typer.Argument(default=None, she
 
     if run_test_deployment:
         deployment_name = project_config.integration_test_deployment
+        if not deployment_name:
+            logging.warning("You have not configured any integration_test_deployment in your configure.py. Exiting.")
+            raise typer.Exit(0)
+        _check_and_run_config_cmd(project_config)
 
     if deployment_name is None:
         logging.error("You have to define an environment to start or use the optional --run-test-deployment!")
@@ -54,14 +59,25 @@ def deployment(deployment_name: Optional[str] = typer.Argument(default=None, she
                        deployment_name)
     logging.debug("Running command to start the environment: %s", " ".join(cmd))
     os.environ['COMPOSE_HTTP_TIMEOUT'] = '300'
-    exit_code = subprocess.call(cmd, cwd=cwd)
+    exit_code = 0
+    try:
+        exit_code = subprocess.call(cmd, cwd=cwd)
+    except KeyboardInterrupt:
+        if not detach:
+            from fastiot.cli.commands.stop import deployment as stop_deployment
+            stop_deployment(deployment_name=deployment_name,
+                            service_names=service_names,
+                            project_name=project_name,
+                            stop_test_deployment=run_test_deployment)
     if exit_code != 0:
         logging.error("Running the environment failed with exit code %s", str(exit_code))
         sys.exit(exit_code)
 
 
 @run_cmd.command(name='tests')
-def run_unittests():
+def run_unittests(start_deployment: Optional[bool] = typer.Option(False,
+                                                                  help="Also start and stop the test-deployment. "
+                                                                       "Defaults to false")):
     """
     This command will trigger all unittests found in the configured test package. Be aware that this will not
     automatically start your integration test deployment with e.g. a message broker. You may start this using the
@@ -71,9 +87,14 @@ def run_unittests():
 
     project_config = get_default_context().project_config
 
-    if project_config.test_package is None:
+    if not project_config.test_package:
         logging.info("No test_package defined in configure.py. Skipping unittests.")
         sys.exit(0)
+
+    _check_and_run_config_cmd(project_config)
+
+    if start_deployment:
+        deployment(run_test_deployment=True, detach=True, project_name=None, service_names=None)
 
     env = os.environ.copy()
     src_dir = os.path.join(project_config.project_root_dir, 'src')
@@ -85,6 +106,21 @@ def run_unittests():
     exit_code = subprocess.call(cmd.split(),
                                 cwd=project_config.project_root_dir,
                                 env=env)
+
+    if start_deployment:
+        from fastiot.cli.commands.stop import deployment as stop_deployment  # pylint: disable=import-outside-toplevel
+        stop_deployment(stop_test_deployment=True, project_name=None, service_names=None)
+
     if exit_code != 0:
         logging.error("Running unittests failed with exit code %s", str(exit_code))
         sys.exit(exit_code)
+
+
+def _check_and_run_config_cmd(project_config):
+    if not os.path.isfile(os.path.join(project_config.project_root_dir, 'src', project_config.test_package,
+                                       'generated.py')):
+        from fastiot.cli.commands.config import config  # pylint: disable=import-outside-toplevel
+
+        logging.warning("No file `generated.py` found in testpackage %s.\n"
+                        "Running config command to create one now.", project_config.test_package)
+        config(test_deployment_only=True, service_port_offset=-1, generated_py_with_internal_hostnames=False)
