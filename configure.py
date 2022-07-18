322b398809223d9297e074c8970aada1c3c2aae5
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-07-18 16:54:58 +0200

    Now also building core modules

--- configure.py
+++ configure.py
@@ -5,7 +5,6 @@ extensions = ['fastiot_ivv_tools.extension']
 project_namespace = 'fastiot'
 
 library_package = 'fastiot'
-services = find_services(package='fastiot_sample_services')
 
 test_package = 'fastiot_tests'
 integration_test_deployment = 'integration_test'
