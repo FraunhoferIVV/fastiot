4e629f537ecdbb56b26c60342486127247790206
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-07-28 10:10:13 +0200

    Allow to inherit from FastIoT infrastructure services and import also those.
    Currently, this is not implemented in a recursive way. So only classes inheriting from InfrastructureService and their direct inheritors will be imported. This already allows services to inherit from services and thus to overwrite certain aspects.

--- src/fastiot/cli/infrastructure_service_fn.py
+++ src/fastiot/cli/infrastructure_service_fn.py
@@ -15,6 +15,8 @@ def get_services_list() -> Dict[str, InfrastructureService]:
     This method will try to import anything from there and for services.
     """
     service_classes = InfrastructureService.__subclasses__()
+    for service_class in service_classes:
+        service_classes += service_class.__subclasses__()
     services = [s() for s in service_classes]
     services =  {s.name: s for s in services}
     return {k: services[k] for k in sorted(services.keys())}
