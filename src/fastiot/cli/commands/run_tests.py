6060687c8f628981eb8836c4cb82bdca7e277709
Konstantin Merker <konstantin.merker@ivv-dd.fraunhofer.de>
2022-09-05 16:05:02 +0200

    Introduced test-runner option for run-tests. This doesn't force the user to install pytest

--- src/fastiot/cli/commands/run_tests.py
+++ src/fastiot/cli/commands/run_tests.py
@@ -1,8 +1,8 @@
+from enum import Enum
 import logging
 import os
 import subprocess
 import sys
-from typing import Optional
 import typer
 from fastiot.cli.commands.start import start
 from fastiot.cli.commands.stop import stop
@@ -10,10 +10,21 @@ from fastiot.cli.model.context import get_default_context
 from fastiot.cli.typer_app import DEFAULT_CONTEXT_SETTINGS, app
 
 
+class TestRunner(str, Enum):
+    """ Specify the test runner """
+    unittest = 'unittest'
+    """ Use python's builtin unittest """
+    pytest = 'pytest'
+    """ Use pytest. This lib must be installed """
+    pytest_cov = 'pytest-cov'
+    """ Use pytest with test coverage option. Please note, that pytest-cov must be installed """
+
+
+
 @app.command(context_settings=DEFAULT_CONTEXT_SETTINGS)
-def run_tests(start_deployment: Optional[bool] = typer.Option(False,
-                                                              help="Also start and stop the test-deployment. "
-                                                                   "Defaults to false")):
+def run_tests(start_deployment: bool = typer.Option(False, help="Also start and stop the test-deployment. Defaults to false"),
+              test_runner: TestRunner = typer.Option(TestRunner.unittest, help="Specify the testrunner to use")
+              ):
     """
     This command will trigger all unittests found in the configured test package. Be aware that this will not
     automatically start your integration test deployment with e.g. a message broker. You may start this using the
@@ -47,18 +58,29 @@ def run_tests(start_deployment: Optional[bool] = typer.Option(False,
     env = os.environ.copy()
     src_dir = os.path.join(project_config.project_root_dir, 'src')
     env['PYTHONPATH'] = src_dir
-
-    cmd = sys.executable + f" -m pytest --rootdir={src_dir} --junitxml=pytest_report.xml " \
-                           f"--cov={src_dir} --cov-report=xml --cov-branch -p no:cacheprovider"
+    cmd = _get_command_for_test_runner(test_runner=test_runner, src_dir=src_dir)
 
     exit_code = subprocess.call(cmd.split(),
                                 cwd=project_config.project_root_dir,
                                 env=env)
 
     if start_deployment:
-        stop(stop_test_deployment=True, project_name=None, service_names=None)
+        stop(use_test_deployment=True, project_name=None, service_names=None)
 
     if exit_code != 0:
         logging.error("Running unittests failed with exit code %s", str(exit_code))
         sys.exit(exit_code)
+
+
+def _get_command_for_test_runner(test_runner: TestRunner, src_dir: str) -> str:
+    if test_runner is TestRunner.unittest:
+        return f"python3 -m unittest discover fastiot_tests"
+    elif test_runner is TestRunner.pytest:
+        return f"python3 -m pytest --rootdir={src_dir} -p no:cacheprovider"
+    elif test_runner is TestRunner.pytest_cov:
+        return f"python3 -m pytest --rootdir={src_dir} --junitxml=pytest_report.xml " \
+            f"--cov={src_dir} --cov-report=xml --cov-branch -p no:cacheprovider"
+    else:
+        raise NotImplementedError()
+
 
