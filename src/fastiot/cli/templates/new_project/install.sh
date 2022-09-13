c7136d2311cb8a9d57b1de0225cabeb50d5e460a
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-09-13 15:11:29 +0200

    Updates to gitignore and project templates

--- src/fastiot/cli/templates/new_project/install.sh
+++ src/fastiot/cli/templates/new_project/install.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-DIR_NAME=$(dirname $0)
-
-if [ -z "$FASTIOT_EXTRA_PYPI" ]; then
-
-python3 -m pip install --extra-index-url https://www.piwheels.org/simple/ \
-    --no-cache-dir -r $DIR_NAME/requirements.txt
-
-else
-
-python3 -m pip install --extra-index-url https://www.piwheels.org/simple/ \
-    --extra-index-url http://${FASTIOT_EXTRA_PYPI} --trusted-host ${FASTIOT_EXTRA_PYPI} \
-    --no-cache-dir -r $DIR_NAME/requirements.txt
-
-fi
-
+../../../../../install.sh
