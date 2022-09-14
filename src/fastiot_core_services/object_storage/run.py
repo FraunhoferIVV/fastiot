8f6adf7629c8f0a2931fc6eac3a0aee053965fc5
Jialiang Yin <jialiang.yin@ivv-dd.fraunhofer.de>
2022-09-14 12:52:21 +0200

    add global logging config file

--- src/fastiot_core_services/object_storage/run.py
+++ src/fastiot_core_services/object_storage/run.py
@@ -1,4 +1,12 @@
+from fastiot.helpers.read_yaml import read_log_config
 from fastiot_core_services.object_storage.object_storage_service import ObjectStorageService
+import logging.config
 
-ObjectStorageService.main()
+if __name__ == '__main__':
+
+    try:
+        logging.config.dictConfig(read_log_config())
+    except ValueError as e:
+        logging.warning('log config file not found')
+    ObjectStorageService.main()
 
