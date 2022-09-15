c2ec3df1165416d0bd2b24dc6a4be3f2dea7d974
Jialiang Yin <jialiang.yin@ivv-dd.fraunhofer.de>
2022-09-15 09:52:29 +0200

    add some comments to log configuration

--- src/fastiot/helpers/log_config.py
+++ src/fastiot/helpers/log_config.py
@@ -3,6 +3,12 @@ from typing import Dict
 
 
 def get_log_config(level: int = logging.INFO) -> Dict:
+    """
+    This function is used to set the logging configuration globally,
+    now the console log should have this format:
+    timestamp: LOG_LEVEL     module name: Log message
+    If the log should be saved to a file, this configuration can also be modified by using logging.Filehandler.
+    """
     return {
         'version': 1,
         'disable_existing_loggers': False,
