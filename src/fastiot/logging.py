360e9046313b930bc8295c66545860887e352975
Jialiang Yin <jialiang.yin@ivv-dd.fraunhofer.de>
2022-09-19 12:16:02 +0200

    add documentation for logging

--- src/fastiot/logging.py
+++ src/fastiot/logging.py
@@ -5,7 +5,10 @@ from fastiot.helpers.log_config import get_log_config
 
 def logging(name: str = None):
     """
-    This logging is a wrapper for logging from python,
+    This logging is a wrapper for logging from python, you can use it like following
+
+    >>> from fastiot import logging
+    >>> logger = logging(name='my_service/my_function')
     """
     import logging.config
     logging.config.dictConfig(get_log_config(env_basic.log_level_no))
