fbf49157ff2390a1e0da912bb75439d6cae5a264
Jialiang Yin <jialiang.yin@ivv-dd.fraunhofer.de>
2022-09-26 15:43:35 +0200

    change nats logger default logging to fastiot logging

--- src/fastiot_core_services/nats_logger/nats_logger_module.py
+++ src/fastiot_core_services/nats_logger/nats_logger_module.py
@@ -1,5 +1,4 @@
-import logging
-
+from fastiot import logging
 from fastiot.core import FastIoTService, Subject, subscribe
 from fastiot_core_services.nats_logger.env import nats_logger_env as env
 
@@ -10,9 +9,8 @@ class NatsLoggerService(FastIoTService):
                                msg_cls=dict))
     async def _on_data_received(self, subject_name: str, msg: dict):
         if env.filter_field is None or str(msg.get(env.filter_field)) == env.filter_value:
-            logging.info("%s: %s", subject_name, str(msg))
+            logging('NatsLoggerService').info("%s: %s", subject_name, str(msg))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     NatsLoggerService.main()
