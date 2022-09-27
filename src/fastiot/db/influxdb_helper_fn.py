245d1a8afc40211121dff80c82a83d6212d6e3fe
Jialiang Yin <jialiang.yin@ivv-dd.fraunhofer.de>
2022-09-27 17:32:17 +0200

    refs #17408, convert synchronous influxdb client to async

--- src/fastiot/db/influxdb_helper_fn.py
+++ src/fastiot/db/influxdb_helper_fn.py
@@ -2,25 +2,25 @@ import logging
 import sys
 import time
 
+import aiohttp
+
 from fastiot.env.env import env_influxdb
 from fastiot.exceptions import ServiceError
 
 
-def get_influxdb_client_from_env():
+async def get_async_influxdb_client_from_env():
     """
     For connecting Influxdb, the environment variables can be set,
     if you want to use your own settings instead of default:
     :envvar:`FASTIOT_INFLUX_DB_HOST`, :envvar:`FASTIOT_INFLUX_DB_PORT`, :envvar:`FASTIOT_INFLUX_DB_TOKEN`
 
-    >>> influxdb_client = get_influxdb_client_from_env()
+    >>> influxdb_client = await get_async_influxdb_client_from_env()
     """
-
     try:
-        # pylint: disable=import-outside-toplevel
-        from influxdb_client import InfluxDBClient
+        from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
         from influxdb_client.client.exceptions import InfluxDBError
     except (ImportError, ModuleNotFoundError):
-        logging.error("You have to manually install `influxdb-client>=1.30,<2` using your `requirements.txt` "
+        logging.error("You have to manually install `influxdb-client[async]>=1.30,<2` using your `requirements.txt` "
                       "to make use of this helper.")
         sys.exit(5)
 
@@ -28,12 +28,12 @@ def get_influxdb_client_from_env():
     num_tries = 300 / sleep_time
     while num_tries > 0:
         try:
-            client = InfluxDBClient(
+            client = InfluxDBClientAsync(
                 url=f"http://{env_influxdb.host}:{env_influxdb.port}",
                 token=env_influxdb.token,
-                org='IVVDD',
+                org='IVVDD'
             )
-            health_check = client.ping()
+            health_check = await client.ping()
             if health_check:
                 return client
             else:
@@ -42,4 +42,5 @@ def get_influxdb_client_from_env():
                 continue
         except InfluxDBError:
             time.sleep(sleep_time)
-    raise ServiceError("Could not connect to InfluxDB")
+        except aiohttp.ServerDisconnectedError:
+            time.sleep(sleep_time)
--- src/fastiot_tests/db/test_databases.py
+++ src/fastiot_tests/db/test_databases.py
@@ -1,6 +1,6 @@
 import unittest
 
-from fastiot.db.influxdb_helper_fn import get_influxdb_client_from_env
+from fastiot.db.influxdb_helper_fn import get_async_influxdb_client_from_env
 from fastiot.db.mariadb_helper_fn import open_mariadb_connection_from_env, init_schema
 from fastiot.db.mongodb_helper_fn import get_mongodb_client_from_env
 from fastiot.db.time_scale_helper_fn import open_timescaledb_connection_from_env
@@ -20,13 +20,20 @@ class TestDataBases(unittest.TestCase):
         init_schema(connection=db_connection, schema='fastiot_fastiotlib')
         self.assertTrue('MariaDB' in db_connection.get_server_info())
 
-    def test_influxdb_connection(self):
-        db_client = get_influxdb_client_from_env()
-        self.assertTrue(db_client.ping())
-
     def test_timescaledb_connection(self):
         db_connection = open_timescaledb_connection_from_env()
         self.assertIsNotNone(db_connection.server_version)
+
+
+class TestDataBasesAsync(unittest.IsolatedAsyncioTestCase):
+
+    async def asyncSetUp(self) -> None:
+        set_test_environment()
+
+    async def test_async_influxdb_connection(self):
+        db_client = await get_async_influxdb_client_from_env()
+        self.assertTrue(await db_client.ping())
+        await db_client.close()
 
 
 if __name__ == '__main__':
