e32b47c3cfab926036c117b9ca4a1f9bf2e02933
Conrad Sieber <conrad.matthias.sieber@ivv-dd.fraunhofer.de>
2022-11-18 11:35:08 +0100

    update documentation

--- src/fastiot_sample_services/redis_producer/__init__.py
+++ src/fastiot_sample_services/redis_producer/__init__.py
@@ -3,7 +3,7 @@
 Example Redis Producer
 =====================
 
-Redis Producer gives an example on how to interact with the :class:RedisHelper'. He saves :class: 'Thing' objects via the
-:class:'RedisHelper' and publishes the Message under :class:'Redis'.
-He also listens to messages published under Redis.>. and gets the corresponding Data from :class:'RedisHelper'.
+Redis Producer gives an example on how to interact with :class:`fastiot.helpers.redis_helper.RedisHelper`. He saves :class:`fastiot.msg.thing.Thing` objects via the
+:class:`fastiot.helpers.redis_helper.RedisHelper` and publishes the Message under :class:`fastiot.msg.redis.RedisMsg`.
+He also listens to messages published under Redis.>. and gets the corresponding Data from :class:`fastiot.helpers.redis_helper.RedisHelper`.
 """
