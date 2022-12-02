00ad3644dd986e5ca30a5468edc2c614899e0268
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2022-12-02 15:28:33 +0100

    Make sure datetime within pydantic models always has a timezone set, refs #17661

--- src/fastiot/core/time.py
+++ src/fastiot/core/time.py
@@ -1,5 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def get_time_now() -> datetime:
     return datetime.utcnow()
+
+
+def ensure_tzinfo(v):
+    # if TZ isn't provided, we assume UTC, but you can do w/e you need
+    if v.tzinfo is None:
+        return v.replace(tzinfo=timezone.utc)
+    # else we convert to utc
+    return v.astimezone(timezone.utc)
