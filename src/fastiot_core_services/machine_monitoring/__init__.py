e8646211684f92ede8156ec1d19f05807edc1f5c
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2023-01-03 19:32:46 +0100

    Preparing documentation for OPC UA logger

--- src/fastiot_core_services/machine_monitoring/__init__.py
+++ src/fastiot_core_services/machine_monitoring/__init__.py
@@ -1,3 +1,12 @@
+"""
+Service to read out OPC UA data
+===============================
+
+For configuration please consult :class:`fastiot_core_services.machine_monitoring.env.OPCUAEnv` and use a CSV-file.
+
+_Documentation to be continued_
+
+"""
 import logging
 
 logging.getLogger('opcua').setLevel(level=logging.ERROR)
--- src/fastiot_core_services/machine_monitoring/env.py
+++ src/fastiot_core_services/machine_monitoring/env.py
@@ -28,27 +28,39 @@ class OPCUAEnv:
 
     @property
     def endpoint_url(self) -> str:
+        """ .. envvar:: FASTIOT_OPCUA_ENDPOINT_URL
+        """
         return os.environ[FASTIOT_OPCUA_ENDPOINT_URL]
 
     @property
     def security_string(self) -> str:
+        """ .. envvar:: FASTIOT_OPCUA_SECURITY_STRING
+        """
         return os.getenv(FASTIOT_OPCUA_SECURITY_STRING, '')
 
     @property
     def user(self) -> str:
+        """ .. envvar:: FASTIOT_OPCUA_USER
+        """
         return os.getenv(FASTIOT_OPCUA_USER, '')
 
     @property
     def password(self) -> str:
+        """ .. envvar:: FASTIOT_OPCUA_PASSWORD
+        """
         return os.getenv(FASTIOT_OPCUA_PASSWORD, '')
 
     @property
     def application_uri(self) -> str:
+        """ .. envvar:: FASTIOT_OPCUA_APPLICATION_URI
+        """
         return os.getenv(FASTIOT_OPCUA_APPLICATION_URI, '')
 
     @property
     def max_allowed_data_delay(self) -> float:
         """
+        .. envvar:: FASTIOT_OPCUA_MAX_ALLOWED_DATA_DELAY
+
         The maximum allowed data delay for opc-ua connections in seconds. If no data changes happen over this timespan,
         the connection to the machine is considered lost. This value must be positive. A value of zero means the
         connection is never considered lost.
@@ -63,6 +75,8 @@ class OPCUAEnv:
     @property
     def polling_delay(self) -> float:
         """
+        .. envvar:: FASTIOT_OPCUA_POLLING_DELAY
+
         The polling delay specifies the delay between polling opcua node cycles. This value is only applied if retrieval
         mode is polling, which is the default behavior, or polling_always. Otherwise this variable is ignored. Must be
         positive. A value of zero means no wait interval which may result in high server load.
@@ -76,15 +90,21 @@ class OPCUAEnv:
 
     @property
     def retrieval_mode(self) -> OPCUARetrievalMode:
+        """ .. envvar:: FASTIOT_OPCUA_RETRIEVAL_MODE
+        """
         return OPCUARetrievalMode(os.getenv(FASTIOT_OPCUA_RETRIEVAL_MODE, OPCUARetrievalMode.polling))
 
     @property
     def machine_monitoring_config_dir(self) -> str:
+        """ .. envvar:: FASTIOT_MACHINE_MONITORING_CONFIG_NAME
+        """
         config_dir_relative = os.getenv(FASTIOT_MACHINE_MONITORING_CONFIG_NAME, "machine_monitoring")
         return os.path.join(env_basic.config_dir, config_dir_relative)
 
     @property
     def machine_monitoring_error_logfile(self) -> str:
+        """ .. envvar:: FASTIOT_MACHINE_MONITORING_ERROR_LOGFILE
+        """
         return os.getenv(FASTIOT_MACHINE_MONITORING_ERROR_LOGFILE, "/var/fastiot/logs/error.log")
 
 
