29c09e016f39bbcd908e07dbd39afe265cc8390a
Konstantin Merker <konstantin.merker@ivv-dd.fraunhofer.de>
2023-01-24 15:59:55 +0100

    added documentation for machine monitoring service

--- src/fastiot_core_services/machine_monitoring/__init__.py
+++ src/fastiot_core_services/machine_monitoring/__init__.py
@@ -4,10 +4,22 @@ Service to read out OPC UA data
 
 For configuration please consult :class:`fastiot_core_services.machine_monitoring.env.OPCUAEnv` and use a CSV-file.
 
-_Documentation to be continued_
+The main idea is to configure the opc-ua nodes via a csv-file and deploy this service to publish the data as
+FastIoT-Things on a nats broker.
 
-TODO: @KM
+Features included:
+ * Running multiple instances with different configurations
+ * Different modes (polling & subscriptions)
+ * A variety of connection params, e.g. security strings
+ * Error handling of opcua connections
 
+Known limitations:
+ * No "auto"-detection of opc-ua nodes supported
+ * OPC-UA nodes must always be specified with nodeid (no browse name support)
+ * No dynamic parsing of opc-ua nodes
+ * No support of opc-ua functions
+ * No support of opc-ua historic requests
+ * Only nats-broker as data output supported
 """
 import logging
 
--- src/fastiot_core_services/machine_monitoring/env.py
+++ src/fastiot_core_services/machine_monitoring/env.py
@@ -30,7 +30,7 @@ class OPCUAEnv:
     def endpoint_url(self) -> str:
         """ .. envvar:: FASTIOT_OPCUA_ENDPOINT_URL
 
-        TODO: @KM
+        The Endpoint Url to the OPC-UA Server, e.g. "opc.tcp://127.0.0.1:5000"
         """
         return os.environ[FASTIOT_OPCUA_ENDPOINT_URL]
 
@@ -38,7 +38,10 @@ class OPCUAEnv:
     def security_string(self) -> str:
         """ .. envvar:: FASTIOT_OPCUA_SECURITY_STRING
 
-        TODO: @KM
+        A security string if its needed, e.g.
+        "Basic256Sha256,Sign,/path/to/certificate/test_certificate.der,/path/to/key/test_key.pem"
+
+        It is recommended to place needed certificates inside a config dir and locate it there.
         """
         return os.getenv(FASTIOT_OPCUA_SECURITY_STRING, '')
 
@@ -46,7 +49,7 @@ class OPCUAEnv:
     def user(self) -> str:
         """ .. envvar:: FASTIOT_OPCUA_USER
 
-        Username for the OPC UA connection
+        Username for the OPC UA connection.
         """
         return os.getenv(FASTIOT_OPCUA_USER, '')
 
@@ -54,7 +57,7 @@ class OPCUAEnv:
     def password(self) -> str:
         """ .. envvar:: FASTIOT_OPCUA_PASSWORD
 
-        Password for the OPC UA Connection
+        Password for the OPC UA Connection.
         """
         return os.getenv(FASTIOT_OPCUA_PASSWORD, '')
 
@@ -62,7 +65,8 @@ class OPCUAEnv:
     def application_uri(self) -> str:
         """ .. envvar:: FASTIOT_OPCUA_APPLICATION_URI
 
-        TODO: @KM
+        In rare cases it is needed to specify an application uri to connect to an OPC-UA Server which can be set with
+        this env variable, e.g. "urn:freeopcua:client"
         """
         return os.getenv(FASTIOT_OPCUA_APPLICATION_URI, '')
 
@@ -102,7 +106,22 @@ class OPCUAEnv:
     def retrieval_mode(self) -> OPCUARetrievalMode:
         """ .. envvar:: FASTIOT_OPCUA_RETRIEVAL_MODE
 
-        TODO: @KM
+        Can be one of 'polling' (default), 'polling_always' or 'subscription'.
+
+        Polling mode requests all OPC-UA nodes in a loop. You can specify the loop delay via
+        'FASTIOT_OPCUA_POLLING_DELAY'. If you use 'polling_always', it will publish a value for each value retrieved.
+        Running in 'polling' will only publish all values in the first run and in consecutive loop runs it will only
+        publish value changes.
+
+        If using subscriptions mode, it will utilize OPC-UA Subscriptions to publish retrieved value changes; depending
+        on the OPC-UA Server implementation. This can be useful when working with many OPC-UA nodes where polling takes
+        too long to complete.
+        Please note that Subscriptions are an optional OPC-UA feature and not all Servers might support it nor might
+        provide a stable implementation. It is recommended when using subscriptions, to also use
+        'FASTIOT_OPCUA_MAX_ALLOWED_DATA_DELAY' to handle connection errors.
+
+        Generally speaking, polling is useful for a small number of nodes <10 and subscriptions for many nodes >1000
+        depending on the usecase. (There is no general answer what is better for values between 10 and 1000.)
         """
         return OPCUARetrievalMode(os.getenv(FASTIOT_OPCUA_RETRIEVAL_MODE, OPCUARetrievalMode.polling))
 
@@ -110,7 +129,9 @@ class OPCUAEnv:
     def machine_monitoring_config_dir(self) -> str:
         """ .. envvar:: FASTIOT_MACHINE_MONITORING_CONFIG_NAME
 
-        TODO: @KM
+        Specify a config name. This can be useful when running multiple instances of machine monitoring with a different
+        opc-ua node configuration inside the config_dir. Defaults to "machine_monitoring" and is relative to the config
+        dir.
         """
         config_dir_relative = os.getenv(FASTIOT_MACHINE_MONITORING_CONFIG_NAME, "machine_monitoring")
         return os.path.join(env_basic.config_dir, config_dir_relative)
@@ -119,7 +140,11 @@ class OPCUAEnv:
     def machine_monitoring_error_logfile(self) -> str:
         """ .. envvar:: FASTIOT_MACHINE_MONITORING_ERROR_LOGFILE
 
-        TODO: @KM
+        Specify an error log file. In this error log file are connection errors logged. If there is any error logged,
+        the machine monitor is marked as unhealthy and depending on the deployment is restarted.
+
+        This can be useful if the OPC-UA Server has unstable subscriptions - so when they break the service is
+        restarted.
         """
         return os.getenv(FASTIOT_MACHINE_MONITORING_ERROR_LOGFILE, "/var/fastiot/logs/error.log")
 
