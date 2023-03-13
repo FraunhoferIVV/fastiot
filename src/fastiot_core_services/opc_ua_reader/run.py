5da62f5410b8427bda997d1e88b2fe8713a0f747
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2023-03-13 17:50:34 +0100

    Update service templates regarding default settings for logging

--- src/fastiot_core_services/opc_ua_reader/run.py
+++ src/fastiot_core_services/opc_ua_reader/run.py
@@ -1,7 +1,4 @@
-import logging
-
 from fastiot_core_services.opc_ua_reader.machine_monitoring_module import OPCUAReader
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     OPCUAReader.main()
