e5cdaa42884a902bf4e786388f25b475f028035a
Volodymyr Drobitko <volodymyr.drobitko@ivv-dd.fraunhofer.de>
2023-03-17 15:34:38 +0100

    Adding integration tests for overwriting in object storage, refs #18367

--- src/fastiot_tests/core_services/test_object_storage.py
+++ src/fastiot_tests/core_services/test_object_storage.py
@@ -21,6 +21,29 @@ from fastiot_core_services.object_storage.object_storage_service import ObjectSt
 
 THING = Thing(machine='SomeMachine', name="RequestSensor", value=42, timestamp=datetime.now(), measurement_id="1")
 
+"""
+test thing overwriting with the following file: 
+sensor_1 sensor_2 measurement_id timestamp
+thing0 thing1 1 t1
+thing2 thing3 1 t2
+thing4 thing5 2 t2
+Converted to messages row by row
+Publish MESSAGES to the message broker
+"""
+MESSAGES = [Thing(machine='test_machine', name='sensor_1', measurement_id='79438k',
+                  value=5, timestamp=datetime(year=2023, month=3, day=16, microsecond=1000)),
+            Thing(machine='test_machine', name='sensor_2', measurement_id='79438k',
+                  value=True, timestamp=datetime(year=2023, month=3, day=16, microsecond=1000)),
+            Thing(machine='test_machine', name='sensor_1', measurement_id='79438k',
+                  value=15, timestamp=datetime(year=2023, month=3, day=16, microsecond=7000)),
+            Thing(machine='test_machine', name='sensor_2', measurement_id='79438k',
+                  value=False, timestamp=datetime(year=2023, month=3, day=16, microsecond=7000)),
+            Thing(machine='test_machine', name='sensor_1', measurement_id='79438d',
+                  value=25.3, timestamp=datetime(year=2023, month=3, day=16, microsecond=7000)),
+            Thing(machine='test_machine', name='sensor_2', measurement_id='79438d',
+                  value='Unknown', timestamp=datetime(year=2023, month=3, day=16, microsecond=7000))
+            ]
+
 
 class FIOTTestValue(FastIoTPublish):
     real: float
@@ -152,9 +175,165 @@ class TestObjectStorage(unittest.IsolatedAsyncioTestCase):
         values = parse_object_list(reply.values, CustomTestMsgList)
         self.assertListEqual(expected_object_list, values)
 
-    #def test_object_overwriting(self):
-    #    await self._start_service()
+    async def test_overwriting_thing_no_change(self):
+        self.get_mongo_col(service_id='u1', collection_name='thing')
+        await self._start_service()
+        self._db_col.delete_many({})
+        # send messages
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check if all objects have been inserted
+        results = list(self._db_col.find({}))
+        self.assertEqual(len(MESSAGES), len(results))
+        # send messages one more time
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check if results are still the same
+        results1 = list(self._db_col.find({}))
+        self.assertListEqual(results, results1)
 
+    async def test_overwriting_thing_only_change(self):
+        self.get_mongo_col(service_id='u1', collection_name='thing')
+        await self._start_service()
+        self._db_col.delete_many({})
+        # send messages
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # modify messages
+        messages = MESSAGES.copy()
+        for idx, msg in enumerate(messages):
+            msg.value = idx*7
+        # send modified messages
+        for msg in messages:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check results
+        results = list(self._db_col.find({}))
+        self.assertEqual(len(MESSAGES), len(results))
+        self.assertEqual(21, results[3]['value'])
+
+    async def test_overwriting_thing_add_data(self):
+        self.get_mongo_col(service_id='u1', collection_name='thing')
+        await self._start_service()
+        self._db_col.delete_many({})
+        # send messages
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # modify messages (add new things)
+        messages = MESSAGES.copy()
+        messages += [Thing(machine='test_machine', name='sensor_1', measurement_id='8000e',
+                           value=35, timestamp=datetime(year=2023, month=3, day=16, microsecond=9000)),
+                     Thing(machine='test_machine', name='sensor_2', measurement_id='8000e',
+                           value=True, timestamp=datetime(year=2023, month=3, day=16, microsecond=9000))]
+        # send modified messages
+        for msg in messages:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check results
+        results = list(self._db_col.find({}))
+        self.assertEqual(len(messages), len(results))
+        self.assertEqual(35, results[len(MESSAGES)]['value'])
+        self.assertEqual(True, results[len(MESSAGES) + 1]['value'])
+
+    async def test_overwriting_thing_changed_data(self):
+        self.get_mongo_col(service_id='u1', collection_name='thing')
+        await self._start_service()
+        self._db_col.delete_many({})
+        # send messages
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # modify messages (update things, removing mustn't affect results)
+        messages = MESSAGES.copy()
+        messages[5].value = True
+        messages[0].value = 0.005
+        messages[0].unit = 'k'
+        messages[4].value = 'Error'
+        del messages[2:4]
+        # send modified messages
+        for msg in messages:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check results
+        results = list(self._db_col.find({}))
+        self.assertEqual(len(MESSAGES), len(results))
+        self.assertEqual(True, results[5]['value'])
+        self.assertEqual(0.005, results[0]['value'])
+        self.assertEqual('k', results[0]['unit'])
+        self.assertEqual('Error', results[4]['value'])
+        self.assertEqual('v1.thing.sensor_1', results[2]['_subject'])
+        self.assertEqual(15, results[2]['value'])
+        self.assertEqual('v1.thing.sensor_2', results[3]['_subject'])
+        self.assertEqual(False, results[3]['value'])
+
+    async def test_overwriting_thing_combined(self):
+        self.get_mongo_col(service_id='u1', collection_name='thing')
+        await self._start_service()
+        self._db_col.delete_many({})
+        # send messages
+        for msg in MESSAGES:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # modify messages
+        messages = MESSAGES.copy()
+        # 1 inserting new things (identified as new)
+        messages.insert(2, Thing(machine='test_machine', name='sensor_1', measurement_id='8000e',
+                                 value=35, timestamp=datetime(year=2023, month=3, day=16, microsecond=9000)))
+        messages.insert(3, Thing(machine='test_machine', name='sensor_2', measurement_id='8000e',
+                                 value=True, timestamp=datetime(year=2023, month=3, day=16, microsecond=9000)))
+        # 2 changing old things (if primary key changed then almost the same as inserting a new thing)
+        messages[0].value = 0.005
+        messages[0].unit = 'k'
+        messages[0].measurement_id = messages[1].measurement_id = '56341f'
+        messages[6].value = 'Error'
+        messages[7].value = True
+        # 3 deleting old things
+        del messages[4:6]
+        # send modified messages
+        for msg in messages:
+            await self.broker_connection.publish(Thing.get_subject(msg.name), msg)
+        await asyncio.sleep(0.02)
+        # check results
+        results = list(self._db_col.find({}))
+        self.assertEqual(10, len(results))
+        self.assertEqual(35, results[8]['value'])
+        self.assertEqual(True, results[9]['value'])
+        self.assertEqual(True, results[5]['value'])
+        self.assertEqual(0.005, results[6]['value'])
+        self.assertEqual('k', results[6]['unit'])
+        self.assertEqual('Error', results[4]['value'])
+        self.assertEqual('v1.thing.sensor_1', results[2]['_subject'])
+        self.assertEqual(15, results[2]['value'])
+        self.assertEqual('v1.thing.sensor_2', results[3]['_subject'])
+        self.assertEqual(False, results[3]['value'])
+
+    async def test_overwriting_object_no_change(self):
+        self.get_mongo_col(service_id='u2', collection_name='custom_test_msg_list')
+        await self._start_service()
+        self._db_col.delete_many({})
+        messages = []
+        for i in range(1, 6):
+            msg_list = [CustomTestMsg(x=FIOTTestValue(real=i, img=2*i),
+                                      y=FIOTTestValue(real=3*i, img=4*i))]
+            test_custom_msg_list = CustomTestMsgList(values=msg_list)
+            messages.append(test_custom_msg_list)
+        # send
+        for msg in messages:
+            await self.broker_connection.publish(msg.get_subject(), msg)
+        await asyncio.sleep(0.02)
+        # check if all objects have been inserted
+        results = list(self._db_col.find({}))
+        self.assertEqual(5, len(results))
+        # send once more
+        for msg in messages:
+            await self.broker_connection.publish(msg.get_subject(), msg)
+        await asyncio.sleep(0.02)
+        # check results
+        self.assertEqual(5, len(results))
 
 if __name__ == '__main__':
     unittest.main()
