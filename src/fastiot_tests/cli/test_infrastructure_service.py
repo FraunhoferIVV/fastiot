6859bcc29ee2f417f202abc58b5eb3e768f087c4
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2023-03-29 13:08:18 +0200

    Fixing unittest for infrastructure service name validation

--- src/fastiot_tests/cli/test_infrastructure_service.py
+++ src/fastiot_tests/cli/test_infrastructure_service.py
@@ -1,3 +1,4 @@
+import gc
 import unittest
 
 from pydantic import ValidationError
@@ -12,8 +13,22 @@ class TestInfrastructureService(unittest.TestCase):
         all_services = InfrastructureService.all.values()
         self.assertTrue('test_service' in [service.name for service in all_services])
 
+    # @unittest.skip("Disturbing other tests")
     def test_infrastructure_service_name_validation(self):
+        invalid_service = None
+
+        class InvalidService(InfrastructureService):
+            name: str = "invalid-service"
+
         with self.assertRaises(ValidationError):
-            class InvalidService(InfrastructureService):
-                name: str ="invalid-service"
-            _ = InvalidService()
+            invalid_service = InvalidService()
+
+        class InvalidService(InfrastructureService):
+            name: str = "now_valid"
+
+        # Overwriting class and forcing garbage collection seems to be the only way not to have an invalid service
+        # left over in memory
+        invalid_service = InvalidService()
+        self.assertTrue(invalid_service.name == "now_valid")
+
+        gc.collect()
