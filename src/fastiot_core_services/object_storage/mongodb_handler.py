b5eae6b9adb610a5a6e9fd3309b2200a31af5335
Tilman Klaeger <tilman.klaeger@ivv-dd.fraunhofer.de>
2023-05-22 15:04:06 +0200

    Lots of small adjustments to documentation

--- src/fastiot_core_services/object_storage/mongodb_handler.py
+++ src/fastiot_core_services/object_storage/mongodb_handler.py
@@ -37,15 +37,15 @@ class MongoDBHandler:
     def create_index(collection: Collection, index: List[Tuple[str, Union[int, Any]]], index_name: str) -> bool:
         """
         Creates the defined index in the defined collection if the index does not exist.
-        Otherwise no changes will be done to the database to save time consuming rebuilding of the index
+        Otherwise, no changes will be done to the database to save time-consuming rebuilding of the index
 
         :param collection: Collection (instance, not name) to create index in
         :param index: Define index as wanted by pymongo create_index, eg. [(column_name, pymongo.ASCENDING)]
-        Instead of pymongo.ASCENDING you can also write 1, DESCENDING is -1
+                      Instead of pymongo.ASCENDING you can also write 1, DESCENDING is -1
         :param index_name: Define a unique name for the index. This name will be used to check if index has already been
-        created
+                           created
+        :returns: True if index has been created, False if index has been created already
 
-        return True if index was created, False if index has been created already
         """
         all_indices = [index['name'] for index in collection.list_indexes()]
         if index_name not in all_indices:
